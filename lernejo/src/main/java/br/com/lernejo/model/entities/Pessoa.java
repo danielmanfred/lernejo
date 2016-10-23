d9702eca1a497318b70befc2dbe3adc481883130
Daniel Manfred <danielmanfred@gmail.com>
2016-10-23 07:26:53 -0300

    Entidade Sexo adicionada.

--- lernejo/src/main/java/br/com/lernejo/model/entities/Pessoa.java
+++ lernejo/src/main/java/br/com/lernejo/model/entities/Pessoa.java
@@ -7,8 +7,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
+import org.hibernate.annotations.ForeignKey;
 
 /**
  * Representa a tabela Pessoa do banco de dados.
@@ -38,6 +40,10 @@ public class Pessoa implements Serializable
     @Column (name = "DataDeMatricula", nullable = false)
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date dataMatricula;
+    
+    @ManyToOne (optional = false)
+    @ForeignKey (name = "PessoaSexo")
+    private Sexo sexo;
 
     public Pessoa() 
     {
